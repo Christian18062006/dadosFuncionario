da0099645ed4b0f0d7fd2056fb3fd980f38b3e5a
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-09 21:46:07 -0300

    iniciado o projeto com git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DadosFuncionários
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
