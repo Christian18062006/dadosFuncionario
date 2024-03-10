77abda0cb13cf789cc6ed79dfbc25fa0187cfefb
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-09 22:10:12 -0300

    adicionada a função de almento de salário E impresso o novo salário na tela. fim do projeto procedural.

--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ namespace DadosFuncionários
 {
     class Program
     {
-        public static double calcularSalario(double salarioBruto, double imposto)
+        public static double calculaSalario(double salarioBruto, double imposto)
         {
             double salarioLiquido = salarioBruto - (salarioBruto * imposto / 100);
             return (salarioLiquido);
         }
+        public static double almentaSalario(double salarioBruto, double quantidadeAlmento)
+        {
+            double almento = (quantidadeAlmento * salarioBruto) / 100;
+            return (almento);
+        }
         static void Main(string[] args)
         {
+            double quantidadeAlmento;
             string nome = string.Empty;
             double salarioBruto;
             double imposto;
@@ -21,9 +27,16 @@ namespace DadosFuncionários
             salarioBruto = double.Parse(Console.ReadLine());
             Console.WriteLine("digite o imposto.");
             imposto = double.Parse(Console.ReadLine());
-            double salarioLiquido = calcularSalario(salarioBruto, imposto);
+            double salarioLiquido = calculaSalario(salarioBruto, imposto);
             Console.Write($"Nome do funcionário: {nome}");
             Console.WriteLine($"Salário líquido: {salarioLiquido.ToString("C2")}");
+            Console.WriteLine("agora, digite a porcentagem que deseja almentar no salário.");
+            quantidadeAlmento = double.Parse(Console.ReadLine());
+            double almento = almentaSalario(salarioBruto, quantidadeAlmento);
+            salarioBruto += almento;
+
+            double novoSalario = calculaSalario(salarioBruto, imposto);
+            Console.WriteLine($"o novo salário é: {novoSalario.ToString("C2")}");
         }
     }
 }
