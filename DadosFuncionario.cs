21c9af9163628c6353a10507ebfb05126cf5aeb8
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-09 22:17:01 -0300

    adicionado método para calcular salário líquido

--- DadosFuncionario.cs
+++ DadosFuncionario.cs
@@ -9,7 +9,11 @@ namespace DadosFuncionários
         public string nome = string.Empty;
         public double salarioBruto;
         public double imposto;
-
+        public double salarioLiquido()
+        {
+            double salarioLiquido = salarioBruto - (salarioBruto * imposto / 100);
+            return (salarioLiquido);
+        }
     }
 
     }
